db283addb9faa00bb5c0b119bc10b71170d67ca9
Rosa Regalado <rosaregalado101@gmail.com>
2022-03-29 00:25:08 -0700

    Users can see all mock events

--- app.js
+++ app.js
@@ -10,10 +10,20 @@ app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allo
 app.set('view engine', 'handlebars');
 
 
-
 // render homepage
+// app.get('/', (req, res) => {
+//   res.render('home', {msg: 'Handlebars cool'});
+// });
+
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+]
+
+// render events page
 app.get('/', (req, res) => {
-  res.render('home', {msg: 'Handlebars cool'});
+  res.render('events-index', { events: events });
 });
 
 const port = process.env.PORT || 3000;
