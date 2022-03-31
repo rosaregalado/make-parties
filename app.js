bfd46c31b8f1459dfcb478d85d546505c3328447
Rosa Regalado <rosaregalado101@gmail.com>
2022-03-30 17:51:18 -0700

    create new event & db

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ const app = express();
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const bodyParser = require('body-parser');
 
 // use "main" as our default layout
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
-// use handlebars to render
+// use handlebars to render html pages
 app.set('view engine', 'handlebars');
-
-
-// render homepage
-// app.get('/', (req, res) => {
-//   res.render('home', {msg: 'Handlebars cool'});
-// });
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var events = [
   { title: "first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
@@ -21,12 +17,23 @@ var events = [
   { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
 
-// render events page
+// render events homepage
 app.get('/', (req, res) => {
   res.render('events-index', { events: events });
+});
+
+// create
+app.post('/events', (req, res) => {
+  console.log(req.body);
+});
+
+// new event
+app.get('/events/new', (req, res) => {
+  res.render('events-new', {});
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('App listening on port 3000!')
 });
+
