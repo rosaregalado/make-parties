9671c4b07cbda758824454c66ceea99cf51b7c06
Rosa Regalado <rosaregalado101@gmail.com>
2022-04-04 23:24:18 -0700

    Users can edit and update events

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars');
+const methodOverride = require('method-override');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 const bodyParser = require('body-parser');
 const models = require('./db/models');
@@ -11,6 +12,8 @@ app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allo
 // use handlebars to render html pages
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
 const path = require('path');
 module.exports = {
@@ -19,13 +22,6 @@ module.exports = {
   "seeders-path": path.resolve('./db/seeders'),
   "migrations-path": path.resolve('./db/migrations')
 };
-
-
-// var events = [
-//   { title: "first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
-//   { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
-//   { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
-// ]
 
 // render events homepage
 app.get('/', (req, res) => {
@@ -55,10 +51,32 @@ app.get('/events/:id', (req, res) => {
     // If the id is for a valid event, show it
     res.render('events-show', { event: event })
   }).catch((err) => {
-    // if they id was for an event not in our db, log an error
+    // if the id was for an event not in our db, log an error
     console.log(err.message);
   })
 })
+
+// edit event
+app.get('/events/:id/edit', (req, res) => {
+  event_id = req.params.id
+  models.Event.findByPk(id=event_id).then(event => {
+    res.render('events-edit', {event: event})
+  })
+});
+
+// update event
+app.put('/events/:id', (req, res) => {
+  models.Event.findByPk(req.params.id).then(event => {
+    event.update(req.body).then(event => {
+      res.redirect(`/events/${req.params.id}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
